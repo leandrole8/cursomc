6339bf63594e56d79f2bd53e7b75787e2d9f4899
leandrole8 <leandro_le8@hotmail.com>
2020-09-05 11:28:11 -0300

    Criado repository e service para Categoria

---
+++ src/main/java/com/leandroreis/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.leandroreis.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leandroreis.cursomc.domain.Categoria;
+import com.leandroreis.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired //instancia automaticamente uma dependencia através da injeção de dependencia ou IoC
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
